57e6580c6006a8a21ce3d9f9ec904295c5cb8472
COTTIN kevin <cottin@informatique.univ-paris-diderot.fr>
2015-12-02 12:12:17 +0100

    complete init_db.sql

--- src/db/init_db.sql
+++ src/db/init_db.sql
@@ -5,13 +5,13 @@ DROP TABLE if exists no_recipe_url;
 DROP TABLE if exists recipe_url;
 DROP TABLE if exists types;
 DROP TABLE if exists recipes;
-DROP TABLE if exists ingredients;#(id*, name)
-DROP TABLE if exists users;#(id*, e-mail, name, birth date)
-DROP TABLE if exists opinions;#(id*, mark, comment, author#, recipe#)
-DROP TABLE if exists recipe_has_ingredients;#(;#(idRecipe#, idIngr#)*)
-DROP TABLE if exists user_currently_has_ingredient;#(;#(idUser#, idIngr#)*)
-DROP TABLE if exists user_has_favorite_ingredient;#(;#(idUser#, idIngr#)*)
-DROP TABLE if exists user_has_forbidden_ingredient;#(;#(idUser#, idIngr#)*)
+DROP TABLE if exists ingredients;
+DROP TABLE if exists users;
+DROP TABLE if exists opinions;
+DROP TABLE if exists recipe_has_ingredients;
+DROP TABLE if exists user_currently_has_ingredient;
+DROP TABLE if exists user_has_favorite_ingredient;
+DROP TABLE if exists user_has_forbidden_ingredient;
 
 CREATE TABLE url_to_treat (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -43,4 +43,58 @@ CREATE TABLE recipes (
 CREATE TABLE types (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE ingredients (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(100)
+);
+
+CREATE TABLE users(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(100),
+	name VARCHAR(100),
+	birth DATE
+);
+
+CREATE TABLE opinions(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	mark INT,
+	comment VARCHAR(100),
+	author INTEGER,
+		FOREIGN KEY (author) REFERENCES users(id),
+	recipe INTEGER,
+		FOREIGN KEY (recipe) REFERENCES recipes(id)
+);
+
+CREATE TABLE recipe_has_ingredients(
+	idRecipe INTEGER,
+	idIngr INTEGER,
+	PRIMARY KEY (idRecipe, idIngr),
+	FOREIGN KEY (idRecipe) REFERENCES recipes(id),
+	FOREIGN KEY (idIngr) REFERENCES ingredients(id)
+);
+
+CREATE TABLE user_currently_has_ingredient(
+	idUser INTEGER,
+	idIngr INTEGER,
+	PRIMARY KEY (idUser, idIngr),
+	FOREIGN KEY (idUser) REFERENCES users(id),
+	FOREIGN KEY (idIngr) REFERENCES ingredients(id)
+);
+
+CREATE TABLE user_has_favorite_ingredient(
+	idUser INTEGER,
+	idIngr INTEGER,
+	PRIMARY KEY (idUser, idIngr),
+	FOREIGN KEY (idUser) REFERENCES users(id),
+	FOREIGN KEY (idIngr) REFERENCES ingredients(id)
+);
+
+CREATE TABLE user_has_forbidden_ingredient(
+	idUser INTEGER,
+	idIngr INTEGER,
+	PRIMARY KEY (idUser, idIngr),
+	FOREIGN KEY (idUser) REFERENCES users(id),
+	FOREIGN KEY (idIngr) REFERENCES ingredients(id)
 );
