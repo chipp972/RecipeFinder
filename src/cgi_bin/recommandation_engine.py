4cee1f66e4126e19c831f04f2deb512bc89beb2e
COTTIN kevin <cottin@informatique.univ-paris-diderot.fr>
2015-12-09 11:26:12 +0100

    delete idea to engine .txt

--- src/cgi_bin/recommandation_engine.py
+++ src/cgi_bin/recommandation_engine.py
@@ -8,9 +8,10 @@ def correct_word(word):
 	@var ingredients string list
 	@return list
 '''
-def get_recipes(recipe_type, ingredients):
+def get_recipes(recipe_type, ingredients, list):
 	# Get datas from database
 	
+	##### get recipe
 	
 	# Compute the result
 	## Compute the weight of ingredients
@@ -24,3 +25,36 @@ def get_recipes(recipe_type, ingredients):
 	## Researched stats
 	
 	# Return the result
+
+'''
+Idea :
+DB :
+	- new relation : "Stats" that regroups
+	 -> its position (rank)
+	 -> if it was clicked
+	 -> id recipe
+	 -> id user
+
+	- add index on user id and recipe id
+
+ R_engine :
+(dico) -> {"78%": "id_recipe1", "53%" : "id2"...}
+
+Algo :
+	Init :
+		Determine the weight of each :
+		-> disliked ingredients of the user
+		-> liked ingredients of the user
+		-> recipes that were chosen before by the user
+			in function of its tastes
+		-> his opinions
+		-> (his favorites)
+		-> recipes that were chosen before by eveybody except user in function of their tastes
+
+	Compute :
+		TFIDF
+
+	Return :
+		Dictionnary with the recipe ids and the result of the tfidf
+
+'''
