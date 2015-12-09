d8c5ace2176b8a6c47ebb5ecee5a9252e5dcbec7
COTTIN kevin <cottin@informatique.univ-paris-diderot.fr>
2015-12-09 10:53:59 +0100

    update recommandation_engine

--- src/cgi_bin/recommandation_engine.py
+++ src/cgi_bin/recommandation_engine.py
@@ -1,28 +1,26 @@
-'''
-	@input:key words, searched options
-	@output:recipes list
-'''
-
-from sklearn.feature_extraction.text import TfidfTransformer
+import sklearn.feature_extraction.text import TfidfTransformer
 
 def correct_word(word):
 	return word
 
-# Entry
-entries = {}
-
-# Get datas from database
-
-
-# Compute the result
-## Compute the weight of ingredients
-transformer = TfidfTransformer()
-tfidf = transformer.fit_transform(entries)
-
-tfidf.toarray()
-
-#-- TF-IDF
-
-## Researched stats
-
-# Return the result
+'''
+	@var recipe_type string
+	@var ingredients string list
+	@return list
+'''
+def get_recipes(recipe_type, ingredients):
+	# Get datas from database
+	
+	
+	# Compute the result
+	## Compute the weight of ingredients
+	transformer = TfidfTransformer()
+	tfidf = transformer.fit_transform(entries)
+	
+	tfidf.toarray()
+	
+	#-- TF-IDF
+	
+	## Researched stats
+	
+	# Return the result
